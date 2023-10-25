46b18be250259292bdf55345b631469411a54dbe
Leon Schumacher <leonsch@protonmail.com>
2023-10-25 11:17:23 +0200

    Add some installer options

--- flake.nix
+++ flake.nix
@@ -27,30 +27,56 @@
                 info() { echo "[32;1m$*[m"; }
                 info "Installing ${name} on ${preConn}"
 
-                info "Placing marker file"
-                file="$(mktemp -p /dev/shm -t und.XXXXXXXX)"
-                (set -x; ssh -t "${preConn}" touch "$file")
-
-                info "Downloading kexec tarball"
-                (set -x; ssh -t "${preConn}" curl -fLOz "$(basename "${kexec}")" "${kexec}")
-
-                info "Unpacking kexec tarbarll"
-                (set -x; ssh -t "${preConn}" tar xvf "$(basename "${kexec}")")
-
-                info "Running kexec"
-                (set -x; ssh -t "${preConn}" sudo ./kexec/run)
-
-                info "Waiting for machine to reboot"
-                while sleep 5; do
-                  (set -x; ssh -o ConnectTimeout=5 "${kexecConn}" test ! -f "$file") && break || true
-                  info "Still waiting"
+                info "Processing options"
+                while (($#)); do
+                  case "$1" in
+                    --no-format)   NO_FORMAT=1;;
+                    --no-kexec)    NO_KEXEC=1;;
+                    --no-reboot)   NO_REBOOT=1;;
+                    --save-hwconf) shift; SAVE_HWCONF="$1";;
+                  esac
+                  shift
                 done
+
+                ((NO_KEXEC)) || {
+                  info "Placing marker file"
+                  file="$(mktemp -p /dev/shm -t und.XXXXXXXX)"
+                  (set -x; ssh -t "${preConn}" touch "$file")
+
+                  info "Downloading kexec tarball"
+                  (set -x; ssh -t "${preConn}" curl -fLOz "$(basename "${kexec}")" "${kexec}")
+
+                  info "Unpacking kexec tarbarll"
+                  (set -x; ssh -t "${preConn}" tar xvf "$(basename "${kexec}")")
+
+                  info "Running kexec"
+                  (set -x; ssh -t "${preConn}" sudo ./kexec/run)
+
+                  info "Waiting for machine to reboot"
+                  while sleep 5; do
+                    (set -x; ssh -o ConnectTimeout=5 "${kexecConn}" test ! -f "$file") && break || true
+                    info "Still waiting"
+                  done
+                }
+
+                [ -n "$SAVE_HWCONF" ] && {
+                  info "Saving hardware configuration to $SAVE_HWCONF"
+                  (set -x; ssh "${kexecConn}" nixos-generate-config \
+                    --show-hardware-config \
+                    --no-filesystems \
+                  | tee "$SAVE_HWCONF")
+
+                  info "You can now edit the hardware configuration!"
+                  read -p "[33;1mPress ENTER when done"
+                }
 
                 info "Uploading flake"
                 (set -x; nix copy "${self}" --to "ssh://${kexecConn}")
 
-                info "Formatting disks"
-                (set -x; ssh -t "${kexecConn}" ${nix} run disko -- -m disko -f "${self}#${name}")
+                ((NO_FORMAT)) || {
+                  info "Formatting disks"
+                  (set -x; ssh -t "${kexecConn}" ${nix} run disko -- -m disko -f "${self}#${name}")
+                }
 
                 info "Installing the system"
                 (set -x; ssh -t "${kexecConn}" nixos-install -v \
@@ -61,8 +87,10 @@
                 info "Exporting all ZFS pools"
                 (set -x; ssh -t "${kexecConn}" zpool export -a)
 
-                info "Rebooting"
-                (set -x; ssh -t "${kexecConn}" reboot)
+                ((NO_REBOOT)) || {
+                  info "Rebooting"
+                  (set -x; ssh -t "${kexecConn}" reboot)
+                }
               ''}";
             };
             "${name}" = {
